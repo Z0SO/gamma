74c93c3d2f9816592e1678221c24291fb99ca49c
Z0SO <lautarorobales31@gmail.com>
2024-04-03 02:27:50 -0300

    add react

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+
+from django.views.generic import TemplateView
+
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+] + static(settings.UPLOADS_URL, document_root=settings.UPLOADS_ROOT)
+
+urlpatterns += [
+    re_path(r'^.*',TemplateView.as_view(template_name='index.html'))
 ]
